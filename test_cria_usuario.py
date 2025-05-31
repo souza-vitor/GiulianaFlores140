3eb0ac9d26052925b88ede629f22342241d9baff
souzavitor <vitorhugovhps@outlook.com>
2025-05-31 08:17:23 -0300

    cleanup nos codigos, falha_autenticar_usuario criado

--- test_cria_usuario.py
+++ test_cria_usuario.py
@@ -3,11 +3,6 @@ import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestCriarusuario():
   def setup_method(self, method):
--- test_falha_autenticar_usuario.py
+++ test_falha_autenticar_usuario.py
@@ -12,15 +12,14 @@ class TestLogarusuario():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_autenticar_usuario(self):
+  def test_falha_autenticar_usuario(self):
     self.driver.get("https://www.giulianaflores.com.br")
     self.driver.set_window_size(1656, 804)
     self.driver.find_element(By.ID, "perfil-hidden").click()
     self.driver.find_element(By.CSS_SELECTOR, "a[href='https://www.giulianaflores.com.br/login.aspx']").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".titulo-dept").text == "IDENTIFICAÇÃO"
     assert self.driver.find_element(By.CSS_SELECTOR, ".txt_newlogin").text == "Olá, digite seus dados:"
-    self.driver.find_element(By.ID, "ContentSite_txtEmail").send_keys("nashton.braxley@dsitip.com")
-    self.driver.find_element(By.ID, "ContentSite_txtPassword").send_keys("SempreAuto10!")
+    self.driver.find_element(By.ID, "ContentSite_txtEmail").send_keys("liz-barbosa92@advogadostb.com.br")
+    self.driver.find_element(By.ID, "ContentSite_txtPassword").send_keys("NadaAuto140!")
     self.driver.find_element(By.ID, "ContentSite_ibtContinue").click()
-    self.driver.find_element(By.ID, "perfil-hidden").click()
-    self.driver.find_element(By.CSS_SELECTOR, "a[href='javascript:Logout();void 0;']").click()
+    assert self.driver.find_element(By.CSS_SELECTOR, "span[class='aviso-erro'] strong").text == "ATENÇÃO"
